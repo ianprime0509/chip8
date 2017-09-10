bd7ea825c136fc0c867042cd04227aea88b4e4ed
Ian Johnson <ianprime0509@gmail.com>
2017-09-09 21:04:10 -0400

    Add LICENSE

--- main.c
+++ main.c
@@ -1,3 +1,19 @@
+/*
+ * This file is part of Chip-8.
+ *
+ * Chip-8 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Chip-8 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Chip-8.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
