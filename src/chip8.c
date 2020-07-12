de1f8ae6a50f00a1834079108a064b95f6566ce5
Ian Johnson <ianprime0509@gmail.com>
2020-07-12 12:04:53 -0400

    Improve formatting of keymap array

--- src/chip8.c
+++ src/chip8.c
@@ -119,8 +119,10 @@ struct surface {
  * z x c v
  */
 SDL_Keycode keymap[16] = {
-    SDLK_x, SDLK_1, SDLK_2, SDLK_3, SDLK_q, SDLK_w, SDLK_e, SDLK_a, SDLK_s,
-    SDLK_d, SDLK_z, SDLK_c, SDLK_4, SDLK_r, SDLK_f, SDLK_v,
+    SDLK_x, SDLK_1, SDLK_2, SDLK_3,
+    SDLK_q, SDLK_w, SDLK_e, SDLK_a,
+    SDLK_s, SDLK_d, SDLK_z, SDLK_c,
+    SDLK_4, SDLK_r, SDLK_f, SDLK_v,
 };
 
 /**
